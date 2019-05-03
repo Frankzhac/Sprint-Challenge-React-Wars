f0399e3692601e3d0dbfab01b294b9e123fd3a3e
Frankzhac <Frank.hemle@gmail.com>
2019-05-03 13:09:06 -0400

    fetching components

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
+import StarWarsCharsList from './components/StarWarsCharsComponents/StarWarsCharsList';
+
 
 class App extends Component {
   constructor() {
--- starwars/src/components/StarWarsChar.js
+++ starwars/src/components/StarWarsChar.js
@@ -38,6 +38,83 @@ class StarWarsChar extends React.Component {
     props.charStarships.forEach(ship => this.fetchShips(ship));
     props.charFilms.forEach(film => this.fetchFilms(film));
   }
+
+  fetchHomeworld = link => {
+    fetch(link)
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        this.setState({
+          homeworld: data.name
+        })
+      })
+      .catch(err => {
+        throw new Error(err);
+      });
+  }
+
+  fetchSpecies = link => {
+    fetch(link)
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        this.setState({
+          species: data.name
+        })
+      })
+      .catch(err => {
+        throw new Error(err);
+      });
+  }
+
+  fetchVehicles = link => {
+    fetch(link)
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        this.setState({
+          vehicles: [...this.state.vehicles, data.name]
+        })
+      })
+      .catch(err => {
+        throw new Error(err);
+      });
+  }
+
+  fetchShips = link => {
+    fetch(link)
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        this.setState({
+          ships: [...this.state.ships, data.name]
+        })
+      })
+      .catch(err => {
+        throw new Error(err);
+      });
+  }
+
+  fetchFilms = link => {
+    fetch(link)
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        this.setState({
+          films: [...this.state.films, `Episode ${data.episode_id}: ${data.title}`].sort((a, b) => +a[8] - +b[8])
+        })
+      })
+      .catch(err => {
+        throw new Error(err);
+      });
+  }
+
+
 };
 
 export default StarWarsChar;
